9d27ecc2e0fdf8b5d6c1d00eee9f32cba9ecd273
Joost Warmerdam <warmerdamjoost@gmail.com>
2024-10-14 21:39:23 +0000

    attempt200

--- reviewSite/reviewSite/settings.py
+++ reviewSite/reviewSite/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$(rs2nxy*p20#@#(hv^vcp_+z0#5#%nrdg0eay^l9fl91g3)ag')
-
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$(rs2nxy*p20#@#(hv^vcp_+z0#5#%nrdg0eay^l9fl91g3)ag')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'

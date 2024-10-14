ebae7b9739a02e920cc77b8d707620d94c74bcb8
Joost Warmerdam <warmerdamjoost@gmail.com>
2024-10-14 20:37:55 +0000

    Make reviewer when new user is made.

--- reviewSite/review_app/apps.py
+++ reviewSite/review_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ReviewAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'review_app'
+    
+    def ready(self):
+        import review_app.signals

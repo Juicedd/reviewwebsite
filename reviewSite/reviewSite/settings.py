3356b1e7ec0a865c3c13b25e7ea723c1e9f59a48
Joost Warmerdam <warmerdamjoost@gmail.com>
2024-10-15 19:25:56 +0000

    update23324324

--- reviewSite/reviewSite/settings.py
+++ reviewSite/reviewSite/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$(rs2nxy*p20#@#(hv^vcp_+z0#5#%nrdg0eay^l9fl91g3)ag')
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$(rs2nxy*p20#@#(hv^vcp_+z0#5#%nrdg0eay^l9fl91g3)ag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'

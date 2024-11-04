1616da331e341083de1dc77fb5d0ec33adc561d5
Joost Warmerdam <warmerdamjoost@gmail.com>
2024-11-04 09:42:38 +0100

    add command to load albums based on csv file (#35)
    * add command to load albums based on csv file
    
    * updated coverart names and get albums with url
    
    * update import albums
    
    * add cover_url to album object when created

--- reviewSite/review_app/management/commands/import_albums.py
+++ reviewSite/review_app/management/commands/import_albums.py
@@ -6,34 +6,22 @@ from review_app.models import Album
 class Command(BaseCommand):
     help = 'Imports albums from a CSV file'
 
+    # require argument
     def add_arguments(self, parser):
         parser.add_argument('csv_file_path', type=str, help='Path to the CSV file')
 
+    # Call the processing function with the added argument
     def handle(self, *args, **kwargs):
         csv_file_path = kwargs['csv_file_path']
         self.import_albums_from_csv(csv_file_path)
 
+    # Process the csv file and write them to album objects.
     def import_albums_from_csv(self, csv_file_path):
         with open(csv_file_path, 'r') as file:
             reader = csv.reader(file)
             next(reader)  # Skip header row if exists
             for row in reader:
-                title, artist, release_date_str = row
-                release_date = datetime.strptime(release_date_str, ' %d-%m-%Y').date()
-                Album.objects.create(title=title, artist=artist, release_date=release_date)
+                title, artist, release_date_str, cover_url = row
+                release_date = datetime.strptime(release_date_str, '%Y-%M-%d').date()
+                Album.objects.create(title=title, artist=artist, release_date=release_date, cover_image=cover_url)
         self.stdout.write(self.style.SUCCESS('Successfully imported albums from CSV'))
-
-
-
-def import_albums_from_csv(file_path):
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in reader:
-            title, artist, release_date = row
-            release_date = datetime.strptime(release_date, '%D-%M-%Y').date()
-            Album.objects.create(title=title, artist=artist, release_date=release_date)
-
-if __name__ == '__main__':
-    file_path = 'C:\\Users\\nicol\\OneDriveJoost\\OneDrive\\Documenten\\Coding\\reviewwebsite\\reviewSite\\review_app\\migrations\\AOTY_albums2023.csv'
-    import_albums_from_csv(file_path)

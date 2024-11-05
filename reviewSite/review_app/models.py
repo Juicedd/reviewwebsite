e4e5125dd1ddcd567785e435e730aa2de8734d7d
Joost Warmerdam <warmerdamjoost@gmail.com>
2024-11-05 08:26:31 +0000

    review_model_migration

--- reviewSite/review_app/models.py
+++ reviewSite/review_app/models.py
@@ -34,17 +34,42 @@ class Reviewer(models.Model):
 
 
 class Review(models.Model):
-    reviewer = models.ForeignKey(Reviewer, on_delete=models.CASCADE, related_name="review_reviewer", null=True, blank=True )
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="reviews_album")
+    reviewer = models.ForeignKey(
+        Reviewer, 
+        on_delete=models.CASCADE, 
+        related_name="reviews", 
+        null=True, 
+        blank=True
+    )
+    album = models.ForeignKey(
+        Album, 
+        on_delete=models.CASCADE, 
+        related_name="reviews"
+    )
     content = models.TextField()
     album_score = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(10)], default=0
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        default=0
     )
     cover_score = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(10)], default=0
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        default=0
     )
-    favorite_track = models.ForeignKey(Track, on_delete=models.SET_NULL)
-    worst_track = models.CharField(max_length=255, null=True, blank=True)
+
+    favorite_track = models.ForeignKey(
+        Track,
+        on_delete=models.SET_NULL,
+        related_name='favorite_review',
+        null=True,
+        blank=True
+    )
+    worst_track = models.ForeignKey(
+        Track,
+        on_delete=models.SET_NULL,
+        related_name='worst_review',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return f"Review by {self.reviewer.name} for Album {self.album.id}"
